f693744686e9b987c35243085ad0ef4e1d028654
Daniel Jr <daniel.souza.lima@outlook.com>
2024-04-18 18:03:25 -0300

    created new editor keymaps

--- lua/keymap/editor.lua
+++ lua/keymap/editor.lua
@@ -6,6 +6,27 @@ vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
 vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
+-- Move in insert mode
+vim.keymap.set("i", "<C-h>", "<left>", { desc = "Move left in insert mode" })
+vim.keymap.set("i", "<C-l>", "<right>", { desc = "Move right in insert mode" })
+vim.keymap.set("i", "<C-j>", "<down>", { desc = "Move down in insert mode" })
+vim.keymap.set("i", "<C-k>", "<up>", { desc = "Move up in insert mode" })
+
+-- Tab control
+vim.keymap.set("n", "<leader>nt", ":tabnew<cr>", { desc = "[N]ew [T]ab" })
+vim.keymap.set("n", "<leader>tc", ":tabclose<cr>", { desc = "[T]ab [C]lose" })
+vim.keymap.set("n", "<Tab>", ":tabnext<cr>", { desc = "Next tab" })
+vim.keymap.set("n", "<S-Tab>", ":tabprevious<cr>", { desc = "Previous tab" })
+
+-- Buffer control
+vim.keymap.set("n", "<leader>bd", ":bd<cr>", { desc = "[B]uffer [D]elete" })
+vim.keymap.set("n", "<S-h>", ":bp<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<S-l>", ":bn<cr>", { desc = "Next buffer" })
+
+-- Error / Warn seek
+vim.keymap.set("n", "g]", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+vim.keymap.set("n", "g[", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+
 -- Filesytem
 vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<cr>", { desc = "Toggle file explorer" })
 
@@ -34,4 +55,3 @@ vim.keymap.set(
 )
 
 vim.keymap.set("n", "<leader>tz", "<cmd>:ZenMode<cr>", { desc = "Toggle Zenmode" })
-
