7cb6dfcc5d32e5a726c422e16926f82311155a03
Daniel Junior <djunior@ionic.health>
2026-05-05 09:12:24 -0300

    fix treesitter API

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,8 +5,8 @@ return { -- Highlight, edit, and navigate code
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
     ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+    require('nvim-treesitter.config').setup {
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'go' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = { enable = true },
